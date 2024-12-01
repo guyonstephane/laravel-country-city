5133e0311e2ab830d2ff60206877b1d2b49f0227
guyonstephane <guyonst@free.fr>
2024-12-01 17:40:36 +0100

    second commit

---
+++ app/Models/Country.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    public function cities()
+
+    {
+
+        return $this->hasMany(City::class);
+
+    }
+}
